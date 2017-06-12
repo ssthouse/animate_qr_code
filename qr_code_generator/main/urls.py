936ea559302ad14f75f7f032db7f8f86aec6fd7a
ssthouse <ssthouse@163.com>
2017-06-12 13:12:07 +0800

    Add database instructions

--- qr_code_generator/main/urls.py
+++ qr_code_generator/main/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import include, url
-from main.views import hello, article
+from main.views import hello, article, crudops
 
 urlpatterns = [
     url(r'^hello/', hello, name='hello'),
-    url(r'^article/(\d+)', article, name='article')
+    url(r'^article/(\d+)', article, name='article'),
+    url(r'^test_db/', crudops, name='test_db')
 ]
--- qr_code_generator/main/views.py
+++ qr_code_generator/main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 import datetime
+from main.models import Dreamreal
 
 # Create your views here.
 
@@ -14,3 +15,36 @@ def hello(request, number=1):
 def article(request, number):
     text = 'Displaying article number: %s' % number
     return HttpResponse(text)
+
+
+def crudops(request):
+    # create an entry
+    dreamreal = Dreamreal(website="https://github.com",
+                          mail="ssthouse@163.com", name="sorex", phonenumber="18771037960")
+    dreamreal.save()
+
+    # read all entries
+    objects = Dreamreal.objects.all()
+    res = 'Printing all Dreamreal entries in the DB : <br>'
+    for elt in objects:
+        res += elt.name + '<br>'
+
+    # read a specific entry
+    sorex = Dreamreal.objects.get(name='sorex')
+    res += "Pringint one result:<br>"
+    res += sorex.name + '<br>'
+
+    # delete one entry
+    res += 'Deleting one entry:<br><br>'
+    sorex.delete()
+
+    # update one entry
+
+    dreamreal = Dreamreal(website="https://github.com",
+                          mail="ssthouse@163.com", name="sorex", phonenumber="18771037960")
+    dreamreal.save()
+    res += "Updating one entry:<br><br>"
+    dreamreal = Dreamreal.objects.get(name='sorex')
+    dreamreal.name = 'ssthouse'
+    dreamreal.save()
+    return HttpResponse(res)
