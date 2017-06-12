1d6e1b2481cba912182dbe0ce2a77533b3881f8d
ssthouse <ssthouse@163.com>
2017-06-12 09:15:02 +0800

    Test using if tag

--- qr_code_generator/main/urls.py
+++ qr_code_generator/main/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url
-from main.views import hello
+from main.views import hello, article
 
 urlpatterns = [
     url(r'^hello/', hello, name='hello'),
+    url(r'^article/(\d+)', article, name='article')
 ]
--- qr_code_generator/main/views.py
+++ qr_code_generator/main/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import datetime
 
 # Create your views here.
 
 
-def hello(request):
-    return render(request, "main/template/hello.html", {})
+def hello(request, number=1):
+    today = datetime.datetime.now().date()
+    return render(request, "main/template/hello.html", {'today': today})
 
 
-# def hello(request, number):
-#     text = "<h1>welcome to my app number %s!</h1>" % number
-#     return HttpResponse(text)
+def article(request, number):
+    text = 'Displaying article number: %s' % number
+    return HttpResponse(text)
